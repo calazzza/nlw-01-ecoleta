9eb43468d0d6438f8f0eaf6212c760d4cb2ab899
Thiago Calazans <calazansthi@gmail.com>
2020-06-07 11:27:27 -0300

    ✨ Finish React frontend
    - clean default React setup
    - add components
    - add routes
    - add styles
    - add backend REST integration
    - add Leaflet map
    - add calls to IBGE API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Routes from './routes'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World. NLW day 1
-        </p>
-        <a
-          className="App-link"
-          href="https://calazzza.github.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          calazzza.github.io
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
